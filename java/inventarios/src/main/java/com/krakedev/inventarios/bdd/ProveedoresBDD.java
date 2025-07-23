030f3bc325d2b3b7fbff1db20191c64e1f48b9f0
joaquinbowen <bowenjoaquinsd@gmail.com>
2025-07-23 10:13:11 -0500

    post inventarios

--- java/inventarios/src/main/java/com/krakedev/inventarios/bdd/ProveedoresBDD.java
+++ java/inventarios/src/main/java/com/krakedev/inventarios/bdd/ProveedoresBDD.java
@@ -6,8 +6,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 import com.krakedev.inventarios.entidades.Proveedor;
+import com.krakedev.inventarios.entidades.TipoDocumento;
 import com.krakedev.inventarios.excepciones.KrakedevException;
 import com.krakedev.inventarios.utils.ConexionBDD;
 
@@ -21,22 +21,26 @@ public class ProveedoresBDD {
 		Proveedor proveedor = null;
 		try {
 			con = ConexionBDD.obtenerConexion();
-			ps = con.prepareStatement("select identificador,tipo_documento,nombre,telefono,correo,direccion"
-					+ " from proveedores"
-					+ " where upper(nombre) like ?");
+			ps = con.prepareStatement("select prov.identificador,tipo_documento,td.descripcion,prov.nombre,prov.telefono,prov.correo,prov.direccion"
+					+ " from proveedores prov, tipo_documento td "
+					+ " where prov.tipo_documento = td.codigo"
+					+ " and upper(nombre) like ?");
 			ps.setString(1, "%"+subcadena.toUpperCase()+"%");
 			rs = ps.executeQuery();
 
 			while (rs.next()) {
 				String identificador = rs.getString("identificador");
-				String tipoDocumento = rs.getString("tipo_documento");
+				String codigoTipoDocumento = rs.getString("tipo_documento");
+				String descripcionTipoDocumento = rs.getString("descripcion");
 				String nombre = rs.getString("nombre");
 				String telefono = rs.getString("telefono");
 				String correo = rs.getString("correo");
 				String direccion =rs.getString("direccion");
-				proveedor = new Proveedor(identificador, tipoDocumento,nombre,telefono,correo,direccion);
+				TipoDocumento tp = new TipoDocumento(codigoTipoDocumento,descripcionTipoDocumento);
+				proveedor = new Proveedor(identificador, tp,nombre,telefono,correo,direccion);
 
 				proveedores.add(proveedor);
+				System.out.println(proveedor);
 
 			}
 
@@ -58,5 +62,38 @@ public class ProveedoresBDD {
 		return proveedores;
 	}
 	
-	
+	public void crear(Proveedor proveedor) throws KrakedevException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		try {
+			con = ConexionBDD.obtenerConexion();
+			ps = con.prepareStatement(
+					"insert into proveedores(identificador,tipo_documento,nombre,telefono,correo,direccion) values(?,?,?,?,?,?)");
+			
+			ps.setString(1, proveedor.getIdentificador());
+			ps.setString(2, proveedor.getTipoDocumento().getCodigo());
+			ps.setString(3, proveedor.getNombre());
+			ps.setString(4, proveedor.getTelefono());
+			ps.setString(5, proveedor.getCorreo());
+			ps.setString(6, proveedor.getDireccion());
+			ps.executeUpdate();
+			
+			
+		} catch (KrakedevException e) {
+			e.printStackTrace();
+			throw e;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new KrakedevException("Error al insertar Detalle:" + e.getMessage());
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
 }
--- java/inventarios/src/main/java/com/krakedev/inventarios/servicios/ServiciosProveedores.java
+++ java/inventarios/src/main/java/com/krakedev/inventarios/servicios/ServiciosProveedores.java
@@ -2,7 +2,9 @@ package com.krakedev.inventarios.servicios;
 
 import java.util.ArrayList;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -31,4 +33,22 @@ public class ServiciosProveedores {
 			return Response.serverError().build();
 		}
 	}
+	
+	@Path("crear")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response crear(Proveedor proveedor){
+		ProveedoresBDD pro = new ProveedoresBDD();
+		try {
+			pro.crear(proveedor);
+			return Response.ok().build();
+		} catch (KrakedevException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
+	}
+	
+	
+	
+	
 }
